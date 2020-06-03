ddc5447855e5f0d2b76ee709a6c52bac1164e372
Fernando Gunawan <flashshadow32@gmail.com>
2020-06-03 14:52:39 +0700

    update logic in script also make it responsive

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
-var video = document.getElementById('video')
+var video = document.getElementById("video");
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('./models')
-]).then(startVideo)
-
+  faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("./models"),
+]).then(startVideo);
 
 function startVideo() {
   navigator.getUserMedia(
-    { video: {}},
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+    { video: {} },
+    (stream) => (video.srcObject = stream),
+    (err) => console.error(err)
+  );
 }
 
-video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  $("#contain").append(canvas);
+  var height = $("#video").height();
+  var width = $("#video").width();
+  const displaySize = { width: width, height: height };
+  faceapi.matchDimensions(canvas, displaySize);
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    const face = "happy"
-    const minProb = 0.7
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections, minProb)
-    console.log(faceapi.extractParams)
-  }, 100)
-})
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    if (detections[0] == null) {
+      $("#result").html("Face Not Found");
+    } else {
+      let happiness = 0;
+      happiness = detections[0].expressions.happy;
+      if (happiness > 0.85) {
+        $("#result").html("You're Seeing Happy :)");
+      } else {
+        $("#result").html("You're Seeing didn't Happy :(");
+      }
+    }
+  }, 2000);
+});
